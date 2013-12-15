07b1f9d9716d8fb06b689721ebd6b7fb6d85f0be
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-16 04:39:12 +0900

    特定ユーザのtweetデータ取得処理追加

--- IrofCryptographic/IrofCryptographic/Model/Twitter/TwitterData.cs
+++ IrofCryptographic/IrofCryptographic/Model/Twitter/TwitterData.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToTwitter;
 
-namespace IrofCryptographic.Twitter
+namespace IrofCryptographic.Model.Twitter
 {
+
+
+
+
     public class TwitterData
     {
+
+        private TwitterContext _context;
+
+        /// <summary>
+        /// Twitterと接続
+        /// </summary>
+        public void Open()
+        {
+            var auth = Authorizer.DoApplicationOnly();
+            auth.Authorize();
+
+            _context = new TwitterContext(auth);
+        }
+
+
+
+
+        public List<LinqToTwitter.Status> GetAllTimeLineData(string targetUserId)
+        {
+            var list = new List<LinqToTwitter.Status>();
+            
+            var auth = Authorizer.DoApplicationOnly();
+
+            auth.Authorize();
+
+            var twitterCtx = new TwitterContext(auth);
+
+            var subscribedList = twitterCtx.Status
+
+                .Where(n => n.Type == StatusType.User)
+                .Where(n => n.ScreenName == targetUserId)
+                .Where(n=>n.Count==200)
+
+                .ToList();
+
+            return subscribedList; 
+        }
+
+
     }
 }
