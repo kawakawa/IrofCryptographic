6230267901a855aadd519329cb17a25e8c120e5a
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-16 03:09:40 +0900

    新規作成

---
+++ IrofCryptographic/IrofCryptographic/Utility/ConfigUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrofCryptographic.Utility
+{
+    class ConfigUtility
+    {
+        public static string GetDataEncrypto(string keyName)
+        {
+            return null;
+        }
+
+
+    }
+}
