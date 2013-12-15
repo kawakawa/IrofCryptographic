582723fb724bd0481feca421cd2ee42f3ffdf2c4
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-16 02:33:27 +0900

    PATH修正

--- IrofCryptographic/IrofCryptographic/View/MainWindow.xaml.cs
+++ IrofCryptographic/IrofCryptographic/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace IrofCryptographic
+namespace IrofCryptographic.View
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
